d013d8b4caaca188faeb160c39f0f40a25665224
BrokenFlows <cianfdowd@gmail.com>
2022-01-12 20:13:50 +0000

    adds mod taps to base layer shifts in Static Keymap

--- keyboards/0xcb/static/keymaps/brokenflows/keymap.c
+++ keyboards/0xcb/static/keymaps/brokenflows/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                        KC_MPLY,
     KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_CA,   KC_AS,   KC_GD,   KC_SF,   KC_G,    KC_H,    KC_SJ,   KC_GK,   KC_AL,            KC_CE,
-    KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSPC,
+    LSFT_T(KC_Z),     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
     KC_LCTL, KC_LALT, KC_LGUI, LT(_FN4, KC_BSPC),TG(_GAME),     LT(_FN3, KC_SPC),    KC_RGUI, KC_LALT, KC_RCTL
 ),
 [_GAME] = LAYOUT_all(
